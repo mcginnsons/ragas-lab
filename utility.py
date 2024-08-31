ed52dac17e5081bc86172ff4a8948616c483265c
Justin McGinnity <jdmcginn@amazon.com>
2024-08-31 16:33:30 -0500

    adding in automatic knowledge base creation

--- utility.py
+++ utility.py
@@ -2,109 +2,19 @@ import json
 import boto3
 import random
 import time
-
+from opensearchpy import OpenSearch, RequestsHttpConnection, AWSV4SignerAuth, RequestError
+from retrying import retry
 
 suffix = random.randrange(200, 900)
 boto3_session = boto3.session.Session()
 region_name = boto3_session.region_name
 iam_client = boto3_session.client('iam')
+aoss_client = boto3.client('opensearchserverless')
 account_number = boto3.client('sts').get_caller_identity().get('Account')
 identity = boto3.client('sts').get_caller_identity()['Arn']
+credentials = boto3.Session().get_credentials()
+bedrock_agent_client = boto3.client('bedrock-agent')
 
-encryption_policy_name = f"bedrock-sample-rag-sp-{suffix}"
-network_policy_name = f"bedrock-sample-rag-np-{suffix}"
-access_policy_name = f'bedrock-sample-rag-ap-{suffix}'
-bedrock_execution_role_name = f'AmazonBedrockExecutionRoleForKnowledgeBase_{suffix}'
-fm_policy_name = f'AmazonBedrockFoundationModelPolicyForKnowledgeBase_{suffix}'
-s3_policy_name = f'AmazonBedrockS3PolicyForKnowledgeBase_{suffix}'
-oss_policy_name = f'AmazonBedrockOSSPolicyForKnowledgeBase_{suffix}'
-
-
-def create_bedrock_execution_role(bucket_name):
-    foundation_model_policy_document = {
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Effect": "Allow",
-                "Action": [
-                    "bedrock:InvokeModel",
-                ],
-                "Resource": [
-                    f"arn:aws:bedrock:{region_name}::foundation-model/amazon.titan-embed-text-v1"
-                ]
-            }
-        ]
-    }
-
-    s3_policy_document = {
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Effect": "Allow",
-                "Action": [
-                    "s3:GetObject",
-                    "s3:ListBucket"
-                ],
-                "Resource": [
-                    f"arn:aws:s3:::{bucket_name}",
-                    f"arn:aws:s3:::{bucket_name}/*"
-                ],
-                "Condition": {
-                    "StringEquals": {
-                        "aws:ResourceAccount": f"{account_number}"
-                    }
-                }
-            }
-        ]
-    }
-
-    assume_role_policy_document = {
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Effect": "Allow",
-                "Principal": {
-                    "Service": "bedrock.amazonaws.com"
-                },
-                "Action": "sts:AssumeRole"
-            }
-        ]
-    }
-    # create policies based on the policy documents
-    fm_policy = iam_client.create_policy(
-        PolicyName=fm_policy_name,
-        PolicyDocument=json.dumps(foundation_model_policy_document),
-        Description='Policy for accessing foundation model',
-    )
-
-    s3_policy = iam_client.create_policy(
-        PolicyName=s3_policy_name,
-        PolicyDocument=json.dumps(s3_policy_document),
-        Description='Policy for reading documents from s3')
-
-    # create bedrock execution role
-    bedrock_kb_execution_role = iam_client.create_role(
-        RoleName=bedrock_execution_role_name,
-        AssumeRolePolicyDocument=json.dumps(assume_role_policy_document),
-        Description='Amazon Bedrock Knowledge Base Execution Role for accessing OSS and S3',
-        MaxSessionDuration=3600
-    )
-
-    # fetch arn of the policies and role created above
-    bedrock_kb_execution_role_arn = bedrock_kb_execution_role['Role']['Arn']
-    s3_policy_arn = s3_policy["Policy"]["Arn"]
-    fm_policy_arn = fm_policy["Policy"]["Arn"]
-
-    # attach policies to Amazon Bedrock execution role
-    iam_client.attach_role_policy(
-        RoleName=bedrock_kb_execution_role["Role"]["RoleName"],
-        PolicyArn=fm_policy_arn
-    )
-    iam_client.attach_role_policy(
-        RoleName=bedrock_kb_execution_role["Role"]["RoleName"],
-        PolicyArn=s3_policy_arn
-    )
-    return bedrock_kb_execution_role
 
 
 def create_oss_policy_attach_bedrock_execution_role(collection_id, bedrock_kb_execution_role):
@@ -138,28 +48,7 @@ def create_oss_policy_attach_bedrock_execution_role(collection_id, bedrock_kb_ex
     return None
 
 
-def create_policies_in_oss(vector_store_name, aoss_client, bedrock_kb_execution_role_arn):
-    encryption_policy = aoss_client.create_security_policy(
-        name=encryption_policy_name,
-        policy=json.dumps(
-            {
-                'Rules': [{'Resource': ['collection/' + vector_store_name],
-                           'ResourceType': 'collection'}],
-                'AWSOwnedKey': True
-            }),
-        type='encryption'
-    )
-
-    network_policy = aoss_client.create_security_policy(
-        name=network_policy_name,
-        policy=json.dumps(
-            [
-                {'Rules': [{'Resource': ['collection/' + vector_store_name],
-                            'ResourceType': 'collection'}],
-                 'AllowFromPublic': True}
-            ]),
-        type='network'
-    )
+def create_access_policy_in_oss(vector_store_name, aoss_client, bedrock_kb_execution_role_arn):
     access_policy = aoss_client.create_access_policy(
         name=access_policy_name,
         policy=json.dumps(
@@ -191,30 +80,7 @@ def create_policies_in_oss(vector_store_name, aoss_client, bedrock_kb_execution_
             ]),
         type='data'
     )
-    return encryption_policy, network_policy, access_policy
-
-
-def delete_iam_role_and_policies():
-    fm_policy_arn = f"arn:aws:iam::{account_number}:policy/{fm_policy_name}"
-    s3_policy_arn = f"arn:aws:iam::{account_number}:policy/{s3_policy_name}"
-    oss_policy_arn = f"arn:aws:iam::{account_number}:policy/{oss_policy_name}"
-    iam_client.detach_role_policy(
-        RoleName=bedrock_execution_role_name,
-        PolicyArn=s3_policy_arn
-    )
-    iam_client.detach_role_policy(
-        RoleName=bedrock_execution_role_name,
-        PolicyArn=fm_policy_arn
-    )
-    iam_client.detach_role_policy(
-        RoleName=bedrock_execution_role_name,
-        PolicyArn=oss_policy_arn
-    )
-    iam_client.delete_role(RoleName=bedrock_execution_role_name)
-    iam_client.delete_policy(PolicyArn=s3_policy_arn)
-    iam_client.delete_policy(PolicyArn=fm_policy_arn)
-    iam_client.delete_policy(PolicyArn=oss_policy_arn)
-    return 0
+    return access_policy
 
 
 def interactive_sleep(seconds: int):
@@ -224,7 +90,6 @@ def interactive_sleep(seconds: int):
         print(dots, end='\r')
         time.sleep(1)
     print('Done!')
-
 
 def create_bedrock_execution_role_multi_ds(bucket_names):
     foundation_model_policy_document = {
@@ -308,3 +173,160 @@ def create_bedrock_execution_role_multi_ds(bucket_names):
         PolicyArn=s3_policy_arn
     )
     return bedrock_kb_execution_role
+
+
+def get_all_roles(iam_client):
+    roles = []
+    paginator = iam_client.get_paginator('list_roles')
+    
+    for page in paginator.paginate():
+        roles.extend(page.get('Roles', []))
+    
+    return roles
+
+def get_matching_roles(role_list, keyword):
+    matching_roles = []
+    for role in role_list:
+        if isinstance(role, dict) and 'RoleName' in role and keyword in role['RoleName']:
+            matching_roles.append(role)
+    return matching_roles
+
+
+
+def create_knowledge_base(index_name, body_json, collection_name, vector_store_name, access_policy_name, embedding_model_arn):   
+    # Use the function
+    all_roles = get_all_roles(iam_client)
+
+    # Use the function
+    matching_roles = get_matching_roles(all_roles, 'AmazonBedrockExecutionRoleForKnowledgeBases')
+
+    bedrock_kb_execution_role_arn = matching_roles[0]["Arn"]
+
+    # create data access policy within OSS
+    try:
+        response = aoss_client.list_access_policies(maxResults=100,type='data')
+        # Check if the specific access policy is present in the response
+        policy_exist = False
+        access_policies = response.get('accessPolicySummaries', [])
+        for policy in access_policies:
+            if policy['name'] == access_policy_name:
+                policy_exist = True
+        if not policy_exist:
+            access_policy = create_access_policy_in_oss(vector_store_name=vector_store_name,
+                        bedrock_kb_execution_role_arn=bedrock_kb_execution_role_arn,
+                        access_policy_name=access_policy_name)
+            interactive_sleep(60)
+    except Exception as e: 
+        print(f'Error while trying to create the access policy, with error {e}')
+
+    
+    # Build the OpenSearch client
+    collections = aoss_client.list_collections()
+    collection = [collection for collection in collections["collectionSummaries"] if collection["name"] == collection_name]
+    awsauth = auth = AWSV4SignerAuth(credentials, region_name, 'aoss')
+    host = collection[0]["id"] + '.' + region_name + '.aoss.amazonaws.com'
+    oss_client = OpenSearch(
+        hosts=[{'host': host, 'port': 443}],
+        http_auth=awsauth,
+        use_ssl=True,
+        verify_certs=True,
+        connection_class=RequestsHttpConnection,
+        timeout=300
+    )
+    
+    # Create index
+    try:
+        indexExists = oss_client.indices.exists(index=index_name)
+        if not indexExists:
+            response = oss_client.indices.create(index=index_name, body=json.dumps(body_json))
+            print('\nCreating index:')
+            print(response)
+
+            # index creation can take up to a minute
+            interactive_sleep(60)
+    except RequestError as e:
+        # you can delete the index if its already exists
+        # oss_client.indices.delete(index=index_name)
+        print(f'Error while trying to create the index, with error {e.error}\nyou may unmark the delete above to delete, and recreate the index')
+    
+    
+    opensearchServerlessConfiguration = {
+            "collectionArn": collection[0]["arn"],
+            "vectorIndexName": index_name,
+            "fieldMapping": {
+                "vectorField": "vector",
+                "textField": "text",
+                "metadataField": "text-metadata"
+            }
+        }
+
+    name = f"{collection_name}-knowledge-base"
+    
+    # Create a KnowledgeBase
+    @retry(wait_random_min=1000, wait_random_max=2000,stop_max_attempt_number=7)
+    def create_knowledge_base_func():
+        create_kb_response = bedrock_agent_client.create_knowledge_base(
+            name = name,
+            roleArn = bedrock_kb_execution_role_arn,
+            knowledgeBaseConfiguration = {
+                "type": "VECTOR",
+                "vectorKnowledgeBaseConfiguration": {
+                    "embeddingModelArn": embedding_model_arn
+                }
+            },
+            storageConfiguration = {
+                "type": "OPENSEARCH_SERVERLESS",
+                "opensearchServerlessConfiguration":opensearchServerlessConfiguration
+            }
+        )
+        return create_kb_response["knowledgeBase"]
+    try:
+        response = bedrock_agent_client.list_knowledge_bases()
+        knowledge_base_exist = False
+        for kb in response['knowledgeBaseSummaries']:
+            if kb['name'] == name:
+                knowledge_base_found = True
+                return kb['knowledgeBaseId']
+            
+        if not knowledge_base_exist:
+            kb = create_knowledge_base_func()
+    except Exception as err:
+        print(f"{err=}, {type(err)=}")
+    
+    return kb['knowledgeBaseId']
+
+
+
+def create_access_policy_in_oss(vector_store_name, bedrock_kb_execution_role_arn, access_policy_name):
+    access_policy = aoss_client.create_access_policy(
+        name=access_policy_name,
+        policy=json.dumps(
+            [
+                {
+                    'Rules': [
+                        {
+                            'Resource': ['collection/' + vector_store_name],
+                            'Permission': [
+                                'aoss:CreateCollectionItems',
+                                'aoss:DeleteCollectionItems',
+                                'aoss:UpdateCollectionItems',
+                                'aoss:DescribeCollectionItems'],
+                            'ResourceType': 'collection'
+                        },
+                        {
+                            'Resource': ['index/' + vector_store_name + '/*'],
+                            'Permission': [
+                                'aoss:CreateIndex',
+                                'aoss:DeleteIndex',
+                                'aoss:UpdateIndex',
+                                'aoss:DescribeIndex',
+                                'aoss:ReadDocument',
+                                'aoss:WriteDocument'],
+                            'ResourceType': 'index'
+                        }],
+                    'Principal': [identity, bedrock_kb_execution_role_arn],
+                    'Description': 'Easy data policy'}
+            ]),
+        type='data'
+    )
+    return access_policy
